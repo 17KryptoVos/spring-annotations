75ea7f54515d246fc5569ec6eb190818ccacc537
17KryptoVos <17kryptovos@protonmail.com>
2020-07-22 13:27:55 +0200

    - Used the @Scope annotation

---
+++ src/com/kryptovos/SpringBeanAnnotateApp.java
@@ -0,0 +1,21 @@
+package com.kryptovos;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringBeanAnnotateApp {
+    public static void main(String[] args) {
+
+        // load spring config
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean
+        Coach theFirstCoach = context.getBean("tennisCoach", Coach.class);
+        Coach theSecondCoach = context.getBean("tennisCoach", Coach.class);
+
+        // execute method
+        boolean result = (theFirstCoach == theSecondCoach);
+        System.out.println("Is it the same? " + result);
+
+        // close bean
+    }
+}
